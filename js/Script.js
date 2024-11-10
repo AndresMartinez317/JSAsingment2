c7e719d7bdcf989025ea6e508c3c97b8468ed599
AndresMartinez317 <58409933+AndresMartinez317@users.noreply.github.com>
2024-11-10 13:58:34 -0500

    Add files via upload

---
+++ js/Script.js
@@ -0,0 +1,35 @@
+function openPurpleImage(){
+    var img = document.getElementById('image');
+    img.src = "images/flowers-purple-large.jpg";
+    var caption = document.getElementById("figcaption");
+    caption.textContent = "Purple flowers ";
+
+}
+function openRedImage(){
+    var img = document.getElementById('image');
+    img.src = "images/flowers-red-large.jpg";
+    var caption = document.getElementById("figcaption");
+    caption.textContent = "Red flowers ";
+
+}
+function openWhiteImage(){
+    var img = document.getElementById('image');
+    img.src = "images/flowers-white-large.jpg"
+    var caption = document.getElementById("figcaption");
+    caption.textContent = "white flowers ";
+
+}
+function openYellowImage(){
+    var img = document.getElementById('image');
+    img.src = "images/flowers-yellow-large.jpg"
+    var caption = document.getElementById("figcaption");
+    caption.textContent = "yellow flowers ";
+
+}
+function openPinkImage(){
+    var img = document.getElementById('image');
+    img.src = "images/flowers-pink-large.jpg"
+    var caption = document.getElementById("figcaption");
+    caption.textContent = "Pink flowers ";
+
+}
